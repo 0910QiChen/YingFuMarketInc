4946fbabe97dad5d61a39c955c8b31649a0734a3
0910QiChen <112992442+0910QiChen@users.noreply.github.com>
2026-03-04 01:55:15 -0500

    icon

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   googleSheets: {
-    apiKey: 'AIzaSyB4wlp8Xuxh4fdSouPiaDR_H3BA8_PsQ9I',
+    apiKey: 'AIzaSyBzMtGSXp_OzcT1fgBHM4A-JoTw_9bFgWM',
     spreadsheetId: '1Sy3AX5JreG-DNEt9flmU0tb4cOdUBXbbVG5nduhpLs0',
   },
 };
